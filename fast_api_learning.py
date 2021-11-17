5e7fcb11d9b0b25cfae1f40dac5f545001191f44
Zulfikar Turganov <vonagrut@gmail.com>
2021-11-17 19:00:09 +0600

    feat(Fast-API): Third lesson of learning FastAPI&Pydantic. Work with pydantic. Creating first models to validate

--- fast_api_learning.py
+++ fast_api_learning.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from schemas import Book
 
 
 app = FastAPI()
@@ -12,3 +13,13 @@ def home():
 @app.get('/{pk}')
 def get_item(pk: int, q: str = None):
     return {"key": pk, "q": q}
+
+
+@app.get('/user/{pk}/items/{item}/')
+def get_user_item(pk: int, item: str):
+    return {"user": pk, "item": item}
+
+
+@app.post('/book')
+def create_book(item: Book):
+    return item
